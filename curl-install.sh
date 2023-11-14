6d7956506cd9f28b619b3cc118b9275460b0d7fd
NethanWin <127036972+NethanWin@users.noreply.github.com>
2023-11-14 22:52:25 +0200

    Create curl-install.sh

---
+++ curl-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Checking if is running in Repo Folder
+if [[ "$(basename "$(pwd)" | tr '[:upper:]' '[:lower:]')" =~ ^scripts$ ]]; then
+    echo "You are running this in ArchNet Folder."
+    echo "Please use ./archnet.sh instead"
+    exit
+fi
+
+# Installing git
+
+echo "Installing git."
+pacman -Sy --noconfirm --needed git glibc
+
+echo "Cloning the ArchNet Project"
+git clone https://github.com/NethanWin/ArchNet
+
+echo "Executing ArchNet Script"
+
+cd $HOME/ArchNet
+
+exec ./archnet.sh
