a4c677b0eadfb80699425951a1c6b4f5100cf094
NethanWin <127036972+NethanWin@users.noreply.github.com>
2023-11-15 21:33:29 +0200

    Update archnet.sh

--- archnet.sh
+++ archnet.sh
@@ -1,38 +1,38 @@
-# timedatectl set-ntp true
+timedatectl set-ntp true
 
-# parted /dev/vda mklabel msdos 
-# parted /dev/vda mkpart primary ext4 1MiB 513MiB
-# parted /dev/vda set 1 boot on
-# parted /dev/vda mkpart primary ext4 513MiB 100%
+parted /dev/vda mklabel msdos 
+parted /dev/vda mkpart primary ext4 1MiB 513MiB
+parted /dev/vda set 1 boot on
+parted /dev/vda mkpart primary ext4 513MiB 100%
 
-# mkfs.ext4 /dev/vda1
-# mkfs.ext4 /dev/vda2
+mkfs.ext4 /dev/vda1
+mkfs.ext4 /dev/vda2
 
-# mount /dev/vda2 /mnt
-# mkdir /mnt/boot
-# mount /dev/vda1 /mnt/boot
+mount /dev/vda2 /mnt
+mkdir /mnt/boot
+mount /dev/vda1 /mnt/boot
 
-# pacstrap /mnt base base-devel linux linux-firmware vim
+pacstrap /mnt base base-devel linux linux-firmware vim
 
-# genfstab -U /mnt >> /mnt/etc/fstab
-# arch-chroot /mnt /bin/bash
-# pacman -S networkmanager grub
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt /bin/bash
+pacman -S networkmanager grub
 
-# systemctl enable NetworkManager
-# grub-install /dev/vda
-# grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable NetworkManager
+grub-install /dev/vda
+grub-mkconfig -o /boot/grub/grub.cfg
 
 #part 2-------------------------------------
-echo "choose a root password:"
-passwd
+# echo "choose a root password:"
+# passwd
 
-sed -i -e '/^#en_US\.UTF-8 UTF-8/s/^#//' -e '/^#en_US ISO-8859-1/s/^#//' /etc/locale.gen
+# sed -i -e '/^#en_US\.UTF-8 UTF-8/s/^#//' -e '/^#en_US ISO-8859-1/s/^#//' /etc/locale.gen
 
-locale-gen
-echo "LANG=en-US.UTF-8" > /etc/locale.conf
-echo "net" > /etc/hostname
-ln -Sf /usr/share/zoneinfo/Asia/Jerusalem /etc/localtime
+# locale-gen
+# echo "LANG=en-US.UTF-8" > /etc/locale.conf
+# echo "net" > /etc/hostname
+# ln -Sf /usr/share/zoneinfo/Asia/Jerusalem /etc/localtime
 
-exit
-unmount -R /mnt
+# exit
+# unmount -R /mnt
 reboot
