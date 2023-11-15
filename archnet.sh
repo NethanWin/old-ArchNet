999b91592ec6a5037201f004001804e72adde30a
NethanWin <127036972+NethanWin@users.noreply.github.com>
2023-11-15 20:22:34 +0200

    Update archnet.sh

--- archnet.sh
+++ archnet.sh
@@ -1,27 +1,37 @@
-if [ $(whoami) = "root"  ]; then
-    groupadd libvirt
-    useradd -m -G wheel,libvirt -s /bin/bash $USERNAME 
-    echo "$USERNAME created, home directory created, added to wheel and libvirt group, default shell set to /bin/bash"
+timedatectl set-ntp true
+# todo auto create partitions hard drives
+lsblk
+cfdisk /dev/vda
+#temp fix ^
 
-# use chpasswd to enter $USERNAME:$password
-    echo "$USERNAME:$PASSWORD" | chpasswd
-    echo "$USERNAME password set"
+mkfs.ext4 /dev/vda1
+mkfs.ext4 /dev/vda2
 
-	cp -R $HOME/ArchTitus /home/$USERNAME/
-    chown -R $USERNAME: /home/$USERNAME/ArchTitus
-    echo "ArchTitus copied to home directory"
+mount /dev/vda2 /mnt
+mkdir /mnt/boot
+mount /dev/vda1 /mnt/boot
 
-# enter $NAME_OF_MACHINE to /etc/hostname
-	echo $NAME_OF_MACHINE > /etc/hostname
-else
-	echo "You are already a user proceed with aur installs"
-fi
-if [[ ${FS} == "luks" ]]; then
-# Making sure to edit mkinitcpio conf if luks is selected
-# add encrypt in mkinitcpio.conf before filesystems in hooks
-    sed -i 's/filesystems/encrypt filesystems/g' /etc/mkinitcpio.conf
-# making mkinitcpio with linux kernel
-    mkinitcpio -p linux
-fi
-echo -ne "
-echo "great succes"
+pacstrap /mnt base base-devel linux linux-firmware vim
+
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt /bin/bash
+pacman -S networkmanager grub
+
+systemctl enable NetworkManager
+grub-install /dev/vda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+passwd
+
+#replace this with an auto command
+vim /etc/locale.gen
+
+
+locale-gen
+echo "LANG=en-US.UTF-8" > /etc/locale.conf
+echo "net" > /etc/hostname
+ln -Sf /usr/share/zoneinfo/Asia/Jerusalem /etc/localtime
+
+exit
+unmount -R /mnt
+reboot
