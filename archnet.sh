3b71e8a9c463d3b84fc7d880f6d627f70998d0df
NethanWin <127036972+NethanWin@users.noreply.github.com>
2023-11-15 21:01:20 +0200

    Auto create partitions

--- archnet.sh
+++ archnet.sh
@@ -1,8 +1,18 @@
 timedatectl set-ntp true
 # todo auto create partitions hard drives
-lsblk
-cfdisk /dev/vda
+
+
+
+#lsblk
+#cfdisk /dev/vda
 #temp fix ^
+
+
+
+parted /dev/vda mklabel msdos 
+parted /dev/vda mkpart primary ext4 1MiB 513MiB
+parted /dev/vda set 1 boot on
+parted /dev/vda mkpart primary ext4 513MiB 100%
 
 mkfs.ext4 /dev/vda1
 mkfs.ext4 /dev/vda2
@@ -11,28 +21,28 @@ mount /dev/vda2 /mnt
 mkdir /mnt/boot
 mount /dev/vda1 /mnt/boot
 
-echo "before pacstrap"
-pacstrap /mnt base base-devel linux linux-firmware vim
+# echo "before pacstrap"
+# pacstrap /mnt base base-devel linux linux-firmware vim
 
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt /bin/bash
-pacman -S networkmanager grub
+# genfstab -U /mnt >> /mnt/etc/fstab
+# arch-chroot /mnt /bin/bash
+# pacman -S networkmanager grub
 
-systemctl enable NetworkManager
-grub-install /dev/vda
-grub-mkconfig -o /boot/grub/grub.cfg
+# systemctl enable NetworkManager
+# grub-install /dev/vda
+# grub-mkconfig -o /boot/grub/grub.cfg
 
-passwd
+# passwd
 
-#replace this with an auto command
-vim /etc/locale.gen
+# #replace this with an auto command
+# vim /etc/locale.gen
 
 
-locale-gen
-echo "LANG=en-US.UTF-8" > /etc/locale.conf
-echo "net" > /etc/hostname
-ln -Sf /usr/share/zoneinfo/Asia/Jerusalem /etc/localtime
+# locale-gen
+# echo "LANG=en-US.UTF-8" > /etc/locale.conf
+# echo "net" > /etc/hostname
+# ln -Sf /usr/share/zoneinfo/Asia/Jerusalem /etc/localtime
 
-exit
-unmount -R /mnt
-reboot
+# exit
+# unmount -R /mnt
+# reboot
