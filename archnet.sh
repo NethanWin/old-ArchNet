af4de9319c29fe2c64e4e41c2181d5f491022ede
NethanWin <127036972+NethanWin@users.noreply.github.com>
2023-11-19 10:18:40 +0200

    Update archnet.sh

--- archnet.sh
+++ archnet.sh
@@ -1,5 +1,6 @@
 timedatectl set-ntp true
-
+# manual partitioning
+# lsblk
 parted /dev/vda mklabel msdos 
 parted /dev/vda mkpart primary ext4 1MiB 513MiB
 parted /dev/vda set 1 boot on
@@ -23,16 +24,27 @@ grub-install /dev/vda
 grub-mkconfig -o /boot/grub/grub.cfg
 
 #part 2-------------------------------------
-# echo "choose a root password:"
-# passwd
+echo "choose a root password:"
+passwd
 
-# sed -i -e '/^#en_US\.UTF-8 UTF-8/s/^#//' -e '/^#en_US ISO-8859-1/s/^#//' /etc/locale.gen
+sed -i -e '/^#en_US\.UTF-8 UTF-8/s/^#//' -e '/^#en_US ISO-8859-1/s/^#//' /etc/locale.gen
 
-# locale-gen
-# echo "LANG=en-US.UTF-8" > /etc/locale.conf
-# echo "net" > /etc/hostname
-# ln -Sf /usr/share/zoneinfo/Asia/Jerusalem /etc/localtime
+locale-gen
+echo "LANG=en-US.UTF-8" > /etc/locale.conf
+echo "net" > /etc/hostname
+ln -Sf /usr/share/zoneinfo/Asia/Jerusalem /etc/localtime
 
-# exit
-# unmount -R /mnt
+exit
+unmount -R /mnt
 reboot
+
+# after base install
+useradd -m name
+# vim /etc/sudoers
+# uncomment group ALL=(ALL:ALL)ALL
+# write "Deafaults !tty_tickets
+
+pacman -S xorg-server xorg-xinit
+pacman -S xorg
+pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter
+sudo systemctl enable lightdm.service
